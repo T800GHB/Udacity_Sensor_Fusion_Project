c8d2ad526e84391f9b40be1747605288d1cde53e
hanbing.guo <hanbing.guo@zongmutech.com>
2019-09-20 10:41:08 +0800

    add kd-tree search utility

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -48,10 +48,38 @@ struct KdTree
 
 	}
 
+	void searchHelper(std::vector<float> target, std::shared_ptr<Node> node, int depth,
+	        float distanceTol, std::vector<int>& ids) {
+	    if (node) {
+
+	        float x_gap = node->point[0] - target[0];
+	        float y_gap = node->point[1] - target[1];
+
+	        if (fabs(x_gap) < distanceTol
+	                and fabs(y_gap) < distanceTol) {
+
+	            float distance = sqrt(x_gap * x_gap + y_gap * y_gap);
+
+                if (distance <= distanceTol) {
+                    ids.emplace_back(node->id);
+                }
+	        }
+
+	        // Check accross boundary
+	        if ((target[depth % 2] - distanceTol) < node->point[depth % 2]) {
+	            searchHelper(target, node->left, depth + 1, distanceTol, ids);
+	        }
+
+	        if ((target[depth % 2] + distanceTol) > node->point[depth % 2]) {
+                searchHelper(target, node->right, depth + 1, distanceTol, ids);
+            }
+	    }
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(target, root, 0, distanceTol, ids);
 		return ids;
 	}
 	
